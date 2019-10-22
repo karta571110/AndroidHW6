99bf3de8f6b628b4799d458e8ed8a170a759dbbb
karta571110 <karta571110@gmail.com>
2019-10-22 16:53:36 +0800

    整理

--- app/src/main/java/com/example/androidhw6/Bpage.java
+++ app/src/main/java/com/example/androidhw6/Bpage.java
@@ -14,29 +14,29 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 public class Bpage extends AppCompatActivity {
-   int BpageColor;
-int BtoAClick=0;
+    int BpageColor;
+    int BtoAClick = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bpage);
-        Intent AtoB=getIntent();
-        BpageColor =AtoB.getIntExtra("bpagecolor",0);
-       final LinearLayout BLayout=(LinearLayout)findViewById(R.id.BLayout);
+        Intent AtoB = getIntent();
+        BpageColor = AtoB.getIntExtra("bpagecolor", 0);
+        final LinearLayout BLayout = (LinearLayout) findViewById(R.id.BLayout);
         final Button BtoA = (Button) findViewById(R.id.BtoA);
         BLayout.setBackgroundColor(BpageColor);
         BtoA.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-               BtoAClick++;
+                BtoAClick++;
 
 
-                Intent BtoA = new Intent(Bpage.this, MainActivity .class);
-                BtoA.putExtra("apagecolor",Color.YELLOW);
-                BtoA.putExtra("BtoAClick",BtoAClick);
+                Intent BtoA = new Intent(Bpage.this, MainActivity.class);
+                BtoA.putExtra("apagecolor", Color.YELLOW);
+                BtoA.putExtra("BtoAClick", BtoAClick);
                 startActivity(BtoA);
             }
         });
-
 
 
     }
--- app/src/main/java/com/example/androidhw6/MainActivity.java
+++ app/src/main/java/com/example/androidhw6/MainActivity.java
@@ -12,21 +12,21 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-int ApageColor=Color.WHITE;
-int BpageColor=Color.BLACK;
-int BtoAClickApage;
+    int ApageColor = Color.WHITE;
+    int BpageColor = Color.BLACK;
+    int BtoAClickApage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent BtoA=getIntent();
-        BtoAClickApage=BtoA.getIntExtra("BtoAClick",0);
+        Intent BtoA = getIntent();
+        BtoAClickApage = BtoA.getIntExtra("BtoAClick", 0);
 
-        final LinearLayout ALayout=(LinearLayout)findViewById(R.id.ALayout);
+        final LinearLayout ALayout = (LinearLayout) findViewById(R.id.ALayout);
         final Button AtoB = (Button) findViewById(R.id.AtoB);
-        if(BtoAClickApage>=1) {
-            ApageColor=BtoA.getIntExtra("apagecolor",0);
+        if (BtoAClickApage >= 1) {
+            ApageColor = BtoA.getIntExtra("apagecolor", 0);
             ALayout.setBackgroundColor(ApageColor);
         }
         ALayout.setBackgroundColor(ApageColor);
@@ -34,7 +34,7 @@ int BtoAClickApage;
             public void onClick(View v) {
 
                 Intent AtoB = new Intent(MainActivity.this, Bpage.class);
-                AtoB.putExtra("bpagecolor",BpageColor);
+                AtoB.putExtra("bpagecolor", BpageColor);
                 startActivity(AtoB);
             }
         });
